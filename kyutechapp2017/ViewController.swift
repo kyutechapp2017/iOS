1f1f9604bc37d820d505646a512cff4249320a3b
tsukasa <q231205t@mail.kyutech.jp>
2017-02-15 19:54:16 +0900

    Initial Commit

---
+++ kyutechapp2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kyutechapp2017
+//
+//  Created by 塩田宰 on 2017/02/15.
+//  Copyright © 2017年 塩田宰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
