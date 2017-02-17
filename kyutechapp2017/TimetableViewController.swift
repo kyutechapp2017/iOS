49742d31ae4c47f1cfeb5b4ecfb5d3c2a5ed5474
3000manJPY <q231205t@mail.kyutech.jp>
2017-02-18 02:05:57 +0900

    #12 曜日のカスタムビューを作成

---
+++ kyutechapp2017/TimetableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimetableViewController.swift
+//  kyutechapp2017
+//
+//  Created by 塩田宰 on 2017/02/17.
+//  Copyright © 2017年 塩田宰. All rights reserved.
+//
+
+import UIKit
+
+class TimetableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
