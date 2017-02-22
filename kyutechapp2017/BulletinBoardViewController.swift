d161d2628fe36ee5d5a13e227b312fa485dd1016
koyamaura <k.kindomail@gmail.com>
2017-02-22 21:12:24 +0900

    #39　グラデーションをボタンすべてに追加

--- kyutechapp2017/BulletinBoardViewController.swift
+++ kyutechapp2017/BulletinBoardViewController.swift
@@ -11,29 +11,25 @@ import UIKit
 class BulletinBoardViewController: UIViewController {
     
     @IBOutlet weak var classesButton: UIButton!
+    @IBOutlet weak var intensiveButton: UIButton!
+    @IBOutlet weak var newsButton: UIButton!
+    @IBOutlet weak var procedureButton: UIButton!
+    @IBOutlet weak var departmentButton: UIButton!
+    @IBOutlet weak var summonButton: UIButton!
+    @IBOutlet weak var studyAbroadButton: UIButton!
+    @IBOutlet weak var scholarshipButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let gradient = CAGradientLayer()
-        gradient.frame = self.view.bounds
-        
-        let color_1 = UIColor(red: 1.0, green: 0.7, blue: 0.7, alpha: 1.0).cgColor
-        let color_2 = UIColor(red: 0.7, green: 1.0, blue: 0.7, alpha: 1.0).cgColor
-        let color_3 = UIColor(red: 0.7, green: 0.7, blue: 1.0, alpha: 1.0).cgColor
-        gradient.colors = [color_1, color_2, color_3]
-        
-        let position_1 = NSNumber(value: 0.0 as Float)
-        let position_2 = NSNumber(value: 0.5 as Float)
-        let position_3 = NSNumber(value: 1.0 as Float)
-        
-        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
-        gradient.locations = [position_1, position_2, position_3]
-//        classesButton.layer.insertSublayer(gradient, at:0)
-        self.view.layer.insertSublayer(gradient, at: 0)
-//        setBackView(Button: classesButton)
-        // Do any additional setup after loading the view.
+        setClassesBackView(Button: classesButton)
+        setIntensiveBackView(Button: intensiveButton)
+        setNewsBackView(Button: newsButton)
+        setProcedureBackView(Button: procedureButton)
+        setDepartmentBackView(Button: departmentButton)
+        setSummonBackView(Button: summonButton)
+        setStudyAbroadBackView(Button: studyAbroadButton)
+        setScholarshipBackView(Button: scholarshipButton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,19 +38,150 @@ class BulletinBoardViewController: UIViewController {
     }
     
     
-    func setBackView(Button: UIButton){
-        let layer = CAGradientLayer()
-        let startColor = UIColor(red: 129, green: 171, blue: 115, alpha: 1)
-        let endColor = UIColor(red: 103, green: 179, blue: 126, alpha: 1)
-        layer.colors = [startColor, endColor]
-        layer.frame = Button.bounds
-        layer.startPoint = CGPoint(x: 0, y: 0)
-        layer.endPoint = CGPoint(x: 1.0, y: 0.8)
-//        Button.layer.addSublayer(layer)
-        Button.layer.insertSublayer(layer, at: 0)
+    func setClassesBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
         
+        let color_1 = UIColor(red: 129/255, green: 171/255, blue: 115/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 103/255, green: 179/255, blue: 126/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
     }
     
+    func setIntensiveBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 0/255, green: 169/255, blue: 157/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 79/255, green: 206/255, blue: 173/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func setNewsBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 244/255, green: 186/255, blue: 78/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 245/255, green: 146/255, blue: 69/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func setProcedureBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 231/255, green: 88/255, blue: 69/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 250/255, green: 111/255, blue: 87/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func setDepartmentBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 85/255, green: 186/255, blue: 225/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 96/255, green: 159/255, blue: 233/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+
+    func setSummonBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 216/255, green: 70/255, blue: 85/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 231/255, green: 88/255, blue: 69/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func setStudyAbroadBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 85/255, green: 192/255, blue: 231/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 101/255, green: 191/255, blue: 189/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func setScholarshipBackView(Button: UIButton){
+        let gradient = CAGradientLayer()
+        gradient.frame = Button.bounds
+        
+        let color_1 = UIColor(red: 253/255, green: 177/255, blue: 123/255, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 234/255, green: 137/255, blue: 191/255, alpha: 1.0).cgColor
+        
+        gradient.colors = [color_1, color_2]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2]
+        Button.layer.insertSublayer(gradient, at:0)
+    }
+
 }
 
 
