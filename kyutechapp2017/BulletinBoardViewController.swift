f8d42233feddbc253810edfdf3fd8e240ad94451
koyamaura <k.kindomail@gmail.com>
2017-02-22 10:29:47 +0900

    #39 グラデーションを背景でテスト：成功

--- kyutechapp2017/BulletinBoardViewController.swift
+++ kyutechapp2017/BulletinBoardViewController.swift
@@ -15,7 +15,24 @@ class BulletinBoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setBackView(Button: classesButton)
+        let gradient = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        
+        let color_1 = UIColor(red: 1.0, green: 0.7, blue: 0.7, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 0.7, green: 1.0, blue: 0.7, alpha: 1.0).cgColor
+        let color_3 = UIColor(red: 0.7, green: 0.7, blue: 1.0, alpha: 1.0).cgColor
+        gradient.colors = [color_1, color_2, color_3]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 0.5 as Float)
+        let position_3 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2, position_3]
+//        classesButton.layer.insertSublayer(gradient, at:0)
+        self.view.layer.insertSublayer(gradient, at: 0)
+//        setBackView(Button: classesButton)
         // Do any additional setup after loading the view.
     }
 
@@ -31,7 +48,11 @@ class BulletinBoardViewController: UIViewController {
         let endColor = UIColor(red: 103, green: 179, blue: 126, alpha: 1)
         layer.colors = [startColor, endColor]
         layer.frame = Button.bounds
-        Button.layer.addSublayer(layer)
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        layer.endPoint = CGPoint(x: 1.0, y: 0.8)
+//        Button.layer.addSublayer(layer)
+        Button.layer.insertSublayer(layer, at: 0)
+        
     }
     
 }
