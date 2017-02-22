6bccf4421ace3f3951621c93d2fbd809c2ad8857
3000manJPY <q231205t@mail.kyutech.jp>
2017-02-22 17:28:47 +0900

    #12 CollectionViewの追加

--- kyutechapp2017/TimetableViewController.swift
+++ kyutechapp2017/TimetableViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class TimetableViewController: UIViewController {
+    
+    @IBOutlet weak var timetableView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.setUp()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +23,64 @@ class TimetableViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+/*
+    Set up --------------------
+*/
+extension TimetableViewController {
+
+    func setUp() {
+        timetableView.delegate = self
+        timetableView.dataSource = self
     }
-    */
+    
+}
+
+
+/*
+    Collectionview --------------------
+*/
+extension TimetableViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    // セクション数の決定
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    // セルの数の決定
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    // セルのサイズを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let tableWidth = (UIScreen.main.bounds.size.width - CGFloat(45)) / CGFloat(5)
+        let tableHeight = (UIScreen.main.bounds.size.height - CGFloat(45)) / CGFloat(6)
+        
+        return CGSize(width: tableWidth, height: tableHeight)
+    }
+    
+    // セルの間の余白を設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    // カスタムセルの紐付け
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "classCell", for: indexPath)
+        return cell
+    }
+    
+    // セルが選ばれたとき
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // segueの呼び出し
+        performSegue(withIdentifier: "toDetailPageVC",sender: nil)
+    }
 
 }
