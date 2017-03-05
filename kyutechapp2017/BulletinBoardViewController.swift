4416793fe6ae6c0f6d20dd2526ca702111d0c373
koyamaura <k.kindomail@gmail.com>
2017-03-05 20:24:11 +0700

    #39　カテゴリーボタンのコード修正

--- kyutechapp2017/BulletinBoardViewController.swift
+++ kyutechapp2017/BulletinBoardViewController.swift
@@ -22,31 +22,17 @@ class BulletinBoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setBackgroundColor(sender: classesButton)
-        setBackgroundColor(sender: intensiveButton)
-        setBackgroundColor(sender: newsButton)
-        setBackgroundColor(sender: procedureButton)
-        setBackgroundColor(sender: departmentButton)
-        setBackgroundColor(sender: summonButton)
-        setBackgroundColor(sender: studyAbroadButton)
-        setBackgroundColor(sender: scholarshipButton)
+        categoryButton(button: classesButton, color_start: .classesColor_1, color_end:.classesColor_2, start_x: 0.0, start_y: 0.0, end_x: 1.0, end_y: 1.0)
+        categoryButton(button: intensiveButton, color_start: .intensiveColor_1, color_end: .intensiveColor_2, start_x: 0.0, start_y: 0.0, end_x: 1.0, end_y: 1.0)
+        categoryButton(button: newsButton, color_start: .news_1, color_end: .news_2, start_x: 1.0, start_y: 0.0, end_x: 0.0, end_y: 1.0)
+        categoryButton(button: procedureButton, color_start: .procedure_1, color_end: .procedure_2, start_x: 0.5, start_y: 0.0, end_x: 0.0, end_y: 0.5)
+        categoryButton(button: departmentButton, color_start: .department_1, color_end: .department_2, start_x: 0.0, start_y: 0.0, end_x: 1.0, end_y: 1.0)
+        categoryButton(button: summonButton, color_start: .summon_1, color_end: .summon_2, start_x: 1.0, start_y: 0.0, end_x: 0.0, end_y: 1.0)
+        categoryButton(button: studyAbroadButton, color_start: .studyAbroad_1, color_end: .studyAbroad_2, start_x: 0.0, start_y: 0.0, end_x: 1.0, end_y: 1.0)
+        categoryButton(button: scholarshipButton, color_start: .scholarship_1, color_end: .scholarship_2, start_x: 0.0, start_y: 0.0, end_x: 1.0, end_y: 1.0)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    func setBackgroundColor(sender: UIButton){
-        let gradient = CAGradientLayer()
-        let position_1 = NSNumber(value: 0.0 as Float)
-        let position_2 = NSNumber(value: 1.0 as Float)
-        var button = CategoryButton()
-        button.setting(button: sender)
-        gradient.colors = [button.color1.cgColor,button.color2.cgColor]
-        gradient.startPoint = button.startPoint
-        gradient.endPoint = button.endPoint
-        gradient.frame = sender.bounds
-        gradient.locations = [position_1, position_2]
-        sender.layer.insertSublayer(gradient, at:0)
     }
 }
--- kyutechapp2017/Color.swift
+++ kyutechapp2017/Color.swift
@@ -8,91 +8,16 @@
 
 import UIKit
 
-enum ButtonTag:Int{
-    case classes = 1
-    case intensive = 2
-    case news = 3
-    case procedure = 4
-    case department = 5
-    case summon = 6
-    case studyAbroad = 7
-    case scholarship = 8
-    
-    static let allValues = [classes,intensive,news,procedure,department,summon,studyAbroad,scholarship]
-    
-    var name: String?{
-        switch self {
-        case .classes: return "授業"
-        case .intensive: return "集中講義"
-        case .news: return "お知らせ"
-        case .procedure: return "各種変更手続き"
-        case .department: return "学科別"
-        case .summon: return "学生呼び出し"
-        case .studyAbroad: return "留学"
-        case .scholarship: return "奨学金"
-        }
-    }
-    
-}
-
-
-struct CategoryButton {
-    var color1: UIColor!
-    var color2: UIColor!
-    var startPoint: CGPoint!
-    var endPoint: CGPoint!
-    var tag: Int?
-    
-    mutating func setting(button: UIButton){
-        for number in ButtonTag.allValues{
-            if button.currentTitle == number.name{
-                tag = number.rawValue
-                break
-            }
-        }
-        switch tag! {
-        case 1:
-            color1 = UIColor.classesColor_1
-            color2 = UIColor.classesColor_2
-            startPoint = CGPoint(x: 0.0, y: 0.0)
-            endPoint = CGPoint(x: 1.0, y: 1.0)
-        case 2: //Intensive
-            color1 = UIColor.intensiveColor_1
-            color2 = UIColor.intensiveColor_2
-            startPoint = CGPoint(x: 1.0, y: 0.0)
-            endPoint = CGPoint(x: 0.0, y: 1.0)
-        case 3: //News
-            color1 = UIColor.news_1
-            color2 = UIColor.news_2
-            startPoint = CGPoint(x: 1.0, y: 0.0)
-            endPoint = CGPoint(x: 0.0, y: 1.0)
-        case 4: //Procedure
-            color1 = UIColor.procedure_1
-            color2 = UIColor.procedure_2
-            startPoint = CGPoint(x: 0.5, y: 0.0)
-            endPoint = CGPoint(x: 0.5, y: 1.0)
-        case 5: //Department
-            color1 = UIColor.department_1
-            color2 = UIColor.department_2
-            startPoint = CGPoint(x: 0.0, y: 0.0)
-            endPoint = CGPoint(x: 1.0, y: 1.0)
-        case 6: //summon
-            color1 = UIColor.summon_1
-            color2 = UIColor.summon_2
-            startPoint = CGPoint(x: 1.0, y: 0.0)
-            endPoint = CGPoint(x: 0.0, y: 1.0)
-        case 7: //studyAbroad
-            color1 = UIColor.studyAbroad_1
-            color2 = UIColor.studyAbroad_2
-            startPoint = CGPoint(x: 0.0, y: 0.0)
-            endPoint = CGPoint(x: 1.0, y: 1.0)
-        case 8: //scholarship
-            color1 = UIColor.scholarship_1
-            color2 = UIColor.scholarship_2
-            startPoint = CGPoint(x: 0.0, y: 0.0)
-            endPoint = CGPoint(x: 1.0, y: 1.0)
-        default:
-            print("color error")
-        }
-    }
+func categoryButton(button: UIButton, color_start:UIColor, color_end:UIColor, start_x:CGFloat, start_y:CGFloat, end_x:CGFloat, end_y:CGFloat){
+    let gradient = CAGradientLayer()
+    let position_1 = NSNumber(value: 0.0 as Float)
+    let position_2 = NSNumber(value: 1.0 as Float)
+    let startPoint = CGPoint(x: start_x, y: start_y)
+    let endPoint = CGPoint(x: end_x, y: end_y)
+    gradient.colors = [color_start.cgColor,color_end.cgColor]
+    gradient.startPoint = startPoint
+    gradient.endPoint = endPoint
+    gradient.frame = button.bounds
+    gradient.locations = [position_1, position_2]
+    button.layer.insertSublayer(gradient, at: 0)
 }
