ca0324eb2ea6667ed8ee7308f32d07c0a84a6c62
KO-YAMAKO <ko.foot1103@gmail.com>
2017-07-21 11:12:04 +0900

    add DetailInfoView.swift

---
+++ kyutechapp2017/DetailInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailInfoView.swift
+//  kyutechapp2017
+//
+//  Created by 山浦功 on 2017/07/21.
+//  Copyright © 2017年 塩田宰. All rights reserved.
+//
+
+import UIKit
+
+class DetailInfoView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+ 
+
+}
